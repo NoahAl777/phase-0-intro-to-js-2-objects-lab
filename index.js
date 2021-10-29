438ec3a789d8ce079e240a4f76a0259664ea604e
Noah <crashlive112@gmail.com>
2021-10-29 10:42:41 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+const employee = {};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  return {
+    ...obj, //create copy of object
+    [key]: value,
+  };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+  const newObj = { ...obj }; //creates copy of object
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+  delete obj[key];
+  return obj;
+}
